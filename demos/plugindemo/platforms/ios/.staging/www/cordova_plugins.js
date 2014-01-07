4223b9856b97b7e35c170e0efeff1f26efc90263
Raymond Camden <raymondcamden@gmail.com>
2014-01-07 10:50:58 -0600

    more

--- demos/plugindemo/platforms/ios/.staging/www/cordova_plugins.js
+++ demos/plugindemo/platforms/ios/.staging/www/cordova_plugins.js
@@ -1,41 +1,5 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.speech.speechsynthesis/www/SpeechSynthesis.js",
-        "id": "org.apache.cordova.speech.speechsynthesis.SpeechSynthesis",
-        "clobbers": [
-            "window.speechSynthesis"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.speech.speechsynthesis/www/SpeechSynthesisUtterance.js",
-        "id": "org.apache.cordova.speech.speechsynthesis.SpeechSynthesisUtterance",
-        "clobbers": [
-            "SpeechSynthesisUtterance"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.speech.speechsynthesis/www/SpeechSynthesisEvent.js",
-        "id": "org.apache.cordova.speech.speechsynthesis.SpeechSynthesisEvent",
-        "clobbers": [
-            "SpeechSynthesisEvent"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.speech.speechsynthesis/www/SpeechSynthesisVoice.js",
-        "id": "org.apache.cordova.speech.speechsynthesis.SpeechSynthesisVoice",
-        "clobbers": [
-            "SpeechSynthesisVoice"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.speech.speechsynthesis/www/SpeechSynthesisVoiceList.js",
-        "id": "org.apache.cordova.speech.speechsynthesis.SpeechSynthesisVoiceList",
-        "clobbers": [
-            "SpeechSynthesisVoiceList"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
--- demos/plugindemo/platforms/ios/www/js/index.js
+++ demos/plugindemo/platforms/ios/www/js/index.js
@@ -4,11 +4,18 @@ document.addEventListener('deviceready', init, false);
 function init() {
     console.log('New init');
     var u = new SpeechSynthesisUtterance();
-    u.text = "You know you're going to make the app say something naughty.";
+    u.text = "Ready to speak.";
     u.lang = 'en-US';
     speechSynthesis.speak(u);
-    console.log('did my speaking');
 
-speechSynthesis.speak(u);
-        
+	var textField = document.querySelector("#text");
+
+	document.querySelector("#doSpeak").addEventListener("touchend", function() {
+		var text = textField.value;
+		var u = new SpeechSynthesisUtterance();
+		u.text = text;
+		u.lang = 'en-US';
+		speechSynthesis.speak(u);
+		
+	},false);
 }
